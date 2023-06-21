fc44138358eabdd9dd25386d88d56b6962c4dd60
Devon Burt <viperman342@hotmail.com>
2023-06-20 20:29:07 -0700

    Added Part 2 own file

--- AdventofCode/src/main/kotlin/AOCDay4Part2.kt
+++ AdventofCode/src/main/kotlin/AOCDay4Part2.kt
@@ -8,37 +8,33 @@ fun main() {
     //  Check if sections overlap
     for (line in arrayOfSections) {
 //    if Overlap increment counter
-        if (checkOverlap(line)) {
+        if (checkOverlap2(line)) {
             counter++
         }
     }
     println(counter)
 }
 
-//  Function reading file into a list and returns the list of strings for each line
-fun readFile(): List<String> {
-    //  Define file location
-    val fileName = "src/main/kotlin/Day4Input.txt"
-    //  Read file in by line
-    return File(fileName).readLines()
-
-
-}
-
 //  Function receives a string and compares the numbers inside string for overlap
-fun checkOverlap(s: String): Boolean {
+fun checkOverlap2(s: String): Boolean {
     //  Split string into parts by ,
     var pair = s.split(',')
     //Check both pairs
-    return checkPairs(pair[0], pair[1])
+    return checkPairs2(pair[0], pair[1])
 }
 
-fun checkPairs(s: String, s1: String): Boolean {
+fun checkPairs2(s: String, s1: String): Boolean {
     //  Split each section by -
     var sectionOne = s.split('-')
     var sectionTwo = s1.split('-')
     //check range
-    return if (checkRange(sectionOne[0].toInt(), sectionOne[1].toInt(), sectionTwo[0].toInt(), sectionTwo[1].toInt())) {
+    return if (checkRange2(
+            sectionOne[0].toInt(),
+            sectionOne[1].toInt(),
+            sectionTwo[0].toInt(),
+            sectionTwo[1].toInt()
+        )
+    ) {
 //        println("$s . $s1")
 //        println("${sectionOne[0]}, ${sectionOne[1]}, ${sectionTwo[0]}, ${sectionTwo[1]}")
         println("OverLap")
@@ -47,7 +43,7 @@ fun checkPairs(s: String, s1: String): Boolean {
 
 }
 
-fun checkRange(oneStart: Int, oneEnd: Int, twoStart: Int, twoEnd: Int): Boolean {
+fun checkRange2(oneStart: Int, oneEnd: Int, twoStart: Int, twoEnd: Int): Boolean {
     //  if overlap ranger [a-b],[x-y] if x >= a & b >=y
     if (oneStart <= twoStart && oneEnd >= twoEnd) {
         return true
